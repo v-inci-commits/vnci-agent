c401b089e966de3e476fad8f41a7b93a1ad098ac
v-inci-commits <v-inci@v-nci.com>
2026-06-29 23:16:22 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const app = express();
 app.use(express.json());
 
 const SHOP = 'cmuabp-uy.myshopify.com';
-const TOKEN = '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';
-const API = 'https://' + SHOP + '/admin/api/2024-01';
+const TOKEN = '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';
+const API = `https://${SHOP}/api/2024-01`;
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -18,10 +18,10 @@ app.all('/shopify/*', async (req, res) => {
   try {
     const path = req.params[0];
     const fetch = (await import('node-fetch')).default;
-    const response = await fetch(API + '/' + path, {
+    const response = await fetch(`${API}/${path}`, {
       method: req.method,
       headers: {
-        'X-Shopify-Access-Token': TOKEN,
+        'X-Shopify-Storefront-Access-Token': TOKEN,
         'Content-Type': 'application/json',
       },
       body: ['GET','HEAD'].includes(req.method) ? undefined : JSON.stringify(req.body),
